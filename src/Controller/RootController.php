02bc4955c896571677e48e5c4af622af1c8300c4
dejf <dawid.rawa@gmail.com>
2025-06-17 09:21:18 +0200

    access control + deletion of some unnecessary files

--- src/Controller/RootController.php
+++ src/Controller/RootController.php
@@ -3,15 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 final class RootController extends AbstractController
 {
+    private const TASKS_TEMPLATE = 'contents/tasks.html.twig';
+    private const NO_ACCESS_TEMPLATE = 'contents/noaccess.html.twig';
+
     #[Route('/', name: 'app_root')]
     public function index(): Response
     {
-        return $this->render('contents/about.html.twig', []);
+        $isAdmin = $this->isGranted('ROLE_ADMIN');
+        $isUser = $this->isGranted('ROLE_USER');
+
+        if ($isAdmin) {
+            return $this->render(self::TASKS_TEMPLATE);
+        } elseif ($isUser) {
+            return $this->render(self::NO_ACCESS_TEMPLATE);
+        }
+
+        return $this->redirectToRoute('app_about');
     }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('contents/about.html.twig');
+    } 
 }
